f7dfd1e268f8293a115fc65b75c18ea77b951157
Rocket5010 <sander.rocket@gmail.com>
2026-06-18 21:35:49 +0200

    Update start-bot.bat

--- start-bot.bat
+++ start-bot.bat
@@ -2,7 +2,7 @@
 REM One-click launcher for the OSRS Flip Bot. Starts the server hidden and
 REM opens the dashboard. Set OSRS_BOT_UA to your own contact before sharing.
 cd /d "%~dp0"
-if "%OSRS_BOT_UA%"=="" set OSRS_BOT_UA=osrs-flip-bot/1.0 (sander.rocket@gmail.com)
+if "%OSRS_BOT_UA%"=="" set OSRS_BOT_UA=osrs-flip-bot/1.0 (rocket5010)
 start "" /b pythonw -m bot.main
 timeout /t 2 /nobreak >nul
 start "" http://127.0.0.1:8000
